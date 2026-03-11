dca8c908baef3031872a61d42551cf7d2009b614
Borimir Ganchev <99996450+BorimirGanchev@users.noreply.github.com>
2026-03-11 11:30:00 +0200

    Revert "adding to main (#14)"
    This reverts commit b46511a6f2d8fe2ac120d7c2833aea4cf0873b75.

--- Backend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'dataProcessing/dataExtraction.js',
-    'dataProcessing/dataProcessing.js'
-  ],
-  coveragePathIgnorePatterns: ['/node_modules/']
-};
