c269a773ee24c8055af240d050b29dcece292e13
Borimir Ganchev <borimir.ganchev@gmail.com>
2026-03-16 19:19:44 +0200

    added test

--- Backend/jest.config.cjs
+++ Backend/jest.config.cjs
@@ -2,8 +2,20 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: [
+    'index.js',
+    'middlewares/authMiddleware.js',
+    'routes/orders.js',
+    'databaseOrders/dbConnection.js',
     'dataProcessing/dataExtraction.js',
     'dataProcessing/dataProcessing.js'
   ],
-  coveragePathIgnorePatterns: ['/node_modules/']
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 99,
+      functions: 99,
+      lines: 99,
+      statements: 99
+    }
+  }
 };
